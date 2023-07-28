7108f06b37ea8040752d690eac5cc2246b92a8d8
Saiju <saijusunny1301@gmail.com>
2023-07-28 15:29:40 +0530

    item details template created

---
+++ NataliyaApp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(User_Registration)
+admin.site.register(item)
+# Register your models here.
---
+++ NataliyaApp/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+from django.contrib.auth.hashers import make_password
+# User Registration Section
+
+SEMESTER_CHOICES = (
+    ("Approved", "Approved"),
+    ("Declin", "Declin"),
+   
+)
+ROLE_CHOICES = (
+    ("user1", "Staff"),
+    ("user2", "User"),
+   
+)
+CATG_CHOICES = (
+    ("Wrap", "Wrap"),
+    ("Icecreem", "Iceceem"),
+    ("Shake", "Shake"),
+   
+)
+class User_Registration(models.Model):
+    
+    name = models.CharField(max_length=255,blank=True,null=True)
+    lastname = models.CharField(max_length=255,blank=True,null=True)
+    nickname = models.CharField(max_length=255,blank=True,null=True)
+    gender = models.CharField(max_length=10,blank=True,null=True)
+    date_of_birth = models.DateField(null=True)
+    phone_number = models.CharField(max_length=255,blank=True,null=True)
+    email = models.EmailField(blank=True,null=True)
+    role = models.CharField(max_length = 255,choices = ROLE_CHOICES,blank=True,null=True)    
+    username = models.CharField(max_length=255,blank=True,null=True)
+    password = models.CharField(max_length=255,blank=True,null=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+    status =models.CharField(max_length = 255,choices = SEMESTER_CHOICES,blank=True,null=True)
+    banner_access = models.BooleanField(default=False)
+    add_item_access = models.BooleanField(default=False)
+    view_order_access = models.BooleanField(default=False)
+    edit_item_access = models.BooleanField(default=False)
+    def _str_(self):
+        return self.nickname
+    
+    def get_email_field_name(self):
+        return 'email'
+
+# Create Artist Profile
+class Profile_User(models.Model):
+    user = models.ForeignKey(User_Registration, on_delete=models.SET_NULL, null=True, blank=True)
+    firstname = models.CharField(max_length=255,blank=True,null=True)
+    lastname = models.CharField(max_length=255,blank=True,null=True)
+    phonenumber = models.CharField(max_length=20)
+    secondnumber = models.CharField(max_length=20)
+    email = models.EmailField(blank=True,null=True)
+    gender = models.CharField(max_length=255,blank=True,null=True)
+    date_of_birth = models.DateField(null=True)
+    address =  models.TextField(blank=True,null=True)
+    pro_pic = models.ImageField(upload_to='images/', default='static/images/default.png')
+
+
+    
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+
+class item(models.Model):
+    user = models.ForeignKey(User_Registration, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=255,blank=True,null=True)
+    description = models.TextField(blank=True,null=True)
+    price = models.FloatField()
+    rating = models.FloatField()
+    category= models.CharField(max_length = 255,choices = CATG_CHOICES,blank=True,null=True) 
+    buying_count = models.IntegerField()
+    offer = models.FloatField()
+    image1 = models.FileField(upload_to='images/items', default='static/images/default.jpg')
+    image2 = models.FileField(upload_to='images/items', default='static/images/default.jpg',blank=True)
+    image3 = models.FileField(upload_to='images/items', default='static/images/default.jpg',blank=True)
+    image4 = models.FileField(upload_to='images/items', default='static/images/default.jpg',blank=True)
+    
+    def __str__(self):
+        return self.name
